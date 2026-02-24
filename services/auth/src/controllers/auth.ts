e423e685bb573b5a59e0e5b4df2202ec072e1de5
Saif <droconos99@gmail.com>
2026-02-24 23:56:40 +0600

    Add: Auth service middlewares

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,38 +1,32 @@
-import { Request, Response } from "express";
 import User from "../model/User.js";
 import jwt from "jsonwebtoken";
+import tryCatch from "../middlewares/tryCatch.js";
 
-export const loginUser = async (req: Request, res: Response) => {
-   try {
-      const { email, name, image } = req.body;
-      let user = await User.findOne({ email });
+export const loginUser = tryCatch(async (req, res) => {
+   const { email, name, image } = req.body;
+   let user = await User.findOne({ email });
 
-      if (!user) {
-         user = await User.create({
-            name,
-            email,
-            image,
-         });
-      }
-
-      const token = jwt.sign(
-         {
-            user,
-         },
-         process.env.JWT_SECRET as string,
-         {
-            expiresIn: "15d",
-         },
-      );
-
-      res.status(200).json({
-         message: "Logged in",
-         token,
-         user,
-      });
-   } catch (error: Error) {
-      res.status(500).json({
-         message: error.message,
+   if (!user) {
+      user = await User.create({
+         name,
+         email,
+         image,
       });
    }
-};
+
+   const token = jwt.sign(
+      {
+         user,
+      },
+      process.env.JWT_SECRET as string,
+      {
+         expiresIn: "15d",
+      },
+   );
+
+   res.status(200).json({
+      message: "Logged in",
+      token,
+      user,
+   });
+});
